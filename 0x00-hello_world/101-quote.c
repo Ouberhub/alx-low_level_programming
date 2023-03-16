90a737d17a1e9027cd00936770dd6ebd59c0ffa7
zakaria ouberraho <z.ouberrahou@gmail.com>
2023-03-16 15:55:21 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
-@@ -13,9 +13,9 @@ long l = 59;
-long fd = 1;
-long syscall = 1;
-long ret = 0;
- __asm__ ("syscall"
-__asm__ ("syscall"
-: "=a" (ret)
-:"a" (syscall),
-: "a" (syscall),
-"D" (fd),
-"S" (s),
-"d" (l));
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
